e2ee5e452217a1f13098cf0623dc74ee3a43cd48
Brian <babta29@gmail.com>
2019-05-20 16:42:15 -0600

    Final with README to practice

--- src/edu/cnm/deepdive/Echo.java
+++ src/edu/cnm/deepdive/Echo.java
@@ -1,11 +1,17 @@
-package edu.cnm.deepdive;
+package edu.cnm.deepdive; //Package name, lower case (sometimes lower_snake_case)
 
-public class Echo {
+public class Echo { //Class Name, UpperCamelCase
 
-  public static void main(String[] args) {
+  private static final String ARG_ITEM_FORMAT = "%d: %s%n"; //constant name UPPER_SNAKE_CASE
 
-    for (int i = 0; i < args.length; i++){
-      System.out.printf("%d: %s%n", i, args[i].toUpperCase());
+  public static void main(String[] args) { //Method name, lowerCamelCase Parameter name, lowerCamelCase
+    //code for making strings Upper or Lowercase dependant of length.
+    for (int i = 0; i < args.length; i++){ // Local variable name, lowerCamelCase
+      if (args[i].length() > 3) {
+        System.out.printf(ARG_ITEM_FORMAT, i, args[i].toUpperCase());
+      } else {
+        System.out.printf(ARG_ITEM_FORMAT, i, args[i].toLowerCase());
+      }
     }
 
   }
---
+++ src/edu/cnm/deepdive/README.md
@@ -0,0 +1,8 @@
+# Echo Sums
+
+## Description 
+
+This is a simple program to takes an array of words and lowercases items with less then three characters and uppercases ones with more then 3. 
+
+## Team members 
+*Brian Alexander
